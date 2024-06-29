161be6387fb4df8d9e7ce981e77ae24279aec770
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-06-29 10:25:48 -0700

    tool responses

--- src/chat.rs
+++ src/chat.rs
@@ -1,6 +1,8 @@
+use std::fmt::Display;
+
 use crate::Result;
 use async_openai::types::{
-    ChatChoice, ChatCompletionRequestMessage, ChatCompletionRequestUserMessageArgs, ChatCompletionTool, ChatCompletionToolArgs, CreateChatCompletionResponse, FunctionObject
+    ChatChoice, ChatCompletionMessageToolCall, ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestMessage, ChatCompletionRequestToolMessageArgs, ChatCompletionRequestUserMessageArgs, ChatCompletionTool, ChatCompletionToolArgs, CreateChatCompletionResponse, FunctionObject
 };
 use serde_json::Value;
 
@@ -9,6 +11,27 @@ pub fn user_msg(content: impl Into<String>) -> Result<ChatCompletionRequestMessa
     let msg = ChatCompletionRequestUserMessageArgs::default()
         .content(content.into())
         .build()?;
+    Ok(msg.into())
+}
+
+pub fn tool_response_msg(
+    tool_call_id: String,
+    content: impl Display,
+) -> Result<ChatCompletionRequestMessage> {
+    let msg = ChatCompletionRequestToolMessageArgs::default()
+        .tool_call_id(tool_call_id)
+        .content(content.to_string())
+        .build()?;
+    Ok(msg.into())
+}
+
+pub fn tool_calls_msg(
+    tool_calls: Vec<ChatCompletionMessageToolCall>,
+) -> Result<ChatCompletionRequestMessage> {
+    let msg = ChatCompletionRequestAssistantMessageArgs::default()
+        .tool_calls(tool_calls)
+        .build()?;
+
     Ok(msg.into())
 }
 
