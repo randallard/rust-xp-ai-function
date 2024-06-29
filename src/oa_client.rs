c807605d67f4ff60fc3d99d5384262eae5b708f4
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-06-28 22:44:41 -0700

    start oa_client

---
+++ src/oa_client.rs
@@ -0,0 +1,9 @@
+use crate::Result;
+use async_openai::{config::OpenAIConfig, Client};
+use std::sync::Arc;
+
+pub type OaClient = Arc<Client<OpenAIConfig>>;
+
+pub fn new_oa_client() -> Result<OaClient> {
+    Ok(Client::new()).into())
+}
